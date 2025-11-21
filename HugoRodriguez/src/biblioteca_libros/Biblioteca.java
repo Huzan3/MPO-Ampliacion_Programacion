988167c6aacf9acc821709ad1d9ee93689fb3639
Huzan3 <huguitorodriguezvera@gmail.com>
2025-11-21 12:43:55 +0100

    Practica 3, ejercicio de array con javadoc incluido

---
+++ HugoRodriguez/src/biblioteca_libros/Biblioteca.java
@@ -0,0 +1,49 @@
+package biblioteca_libros;
+
+import java.util.ArrayList;
+
+/**
+ * Clase Biblioteca
+ *
+ */
+public class Biblioteca {
+    private ArrayList<Libro> catalogo;
+
+    public Biblioteca() {catalogo = new ArrayList<>(); }
+
+    public ArrayList<Libro> getCatalogo() { return catalogo; }
+
+    public void setCatalogo(ArrayList<Libro> catalogo) { this.catalogo = catalogo; }
+
+    //Metodos
+
+    /**
+     * Metodo para añadir libros a la ArrayList
+     * @param libro establece el objeto libro
+     */
+    public void addLibro(Libro libro) {
+        catalogo.add(libro);
+    }
+
+    /**
+     *  Metodo para que te muestra los libros guardados en la ArrayList
+     */
+    public void mostrarLibros() {
+        for (Libro libro : catalogo) {
+            System.out.println(libro);
+        }
+    }
+
+    /**
+     *  Metodo para buscar libros en funcion del autor estipulado
+     *  si el autor introducido coincide con el de la Array printea el libro
+     * @param autor determina el nombre del autor
+     */
+    public void buscarPorAutor(String autor) {
+        for (Libro libro : catalogo) {
+            if (libro.getAutor().equals(autor)) {
+                System.out.println(libro);
+            }
+        }
+    }
+}
